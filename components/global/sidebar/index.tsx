47d74d921cb9a9045f49e4bc7b92ebfabf2f5e6a
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-14 10:57:31 +0530

    user signin done

--- components/global/sidebar/index.tsx
+++ components/global/sidebar/index.tsx
@@ -4,6 +4,9 @@ import { usePaths } from "@/hooks/use-nav";
 import { LogoSmall } from "@/svgs/logo-small";
 import React from "react";
 import Items from "./items";
+import { Separator } from "@/components/ui/separator";
+import { HelpDuoToneWhite } from "@/icons";
+import ClerkAuthState from "../clerk-auth-state";
 
 type Props = {
   slug: string;
@@ -16,10 +19,26 @@ const Sidebar = ({ slug }: Props) => {
       <div className="flex flex-col gap-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
         <div className="flex gap-x-2 items-center p-5 justify-center">
           <LogoSmall />
-          <div className="flex flex-col py-3">
-            <Items page={page} slug={slug} />
+        </div>
+        <div className="flex flex-col py-3">
+          <Items page={page} slug={slug} />
+        </div>
+        <div className="px-16">
+          <Separator orientation="horizontal" className="bg-[#333336]" />
+        </div>
+
+        <div className="px-3 flex flex-col gap-y-5">
+          <div className="flex gap-x-2">
+            <ClerkAuthState />
+            <p className="text-[#9B9CA0">Profile</p>
+          </div>
+          <div className="flex gap-x-3">
+            <HelpDuoToneWhite />
+            <p className="text-[#9B9CA0">Help</p>
           </div>
         </div>
+
+
       </div>
     </div>
   );
