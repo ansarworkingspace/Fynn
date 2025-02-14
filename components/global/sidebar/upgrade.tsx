5ff03e8b7170ecf8aa4c5309894fcf3b75d0a0a0
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-14 20:20:39 +0530

    dev

--- components/global/sidebar/upgrade.tsx
+++ components/global/sidebar/upgrade.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
+import PaymentButton from "../payment-button";
 
-type Props = {}
+type Props = {};
 
 const UpgradeCard = (props: Props) => {
   return (
-    <div className='bg-[#252525] p-3 flex flex-col gap-y-3'>
-<span className='text-sm'>
-    Upgrade to {''}
-    <span className='bg-gradient-to-r from-[#CC3BD4] to-[#D064AC] font-bold bg-clip-text text-transparent'>
-        Smart AI
-    </span>
-</span>
-<p className='text-[#989CA0] font-light text-sm'>
-    Unlock all features <br/> including AI and more
-</p>
+    <div className="bg-[#252525] p-3 flex flex-col gap-y-3 rounded-xl">
+      <span className="text-sm">
+        Upgrade to {""}
+        <span className="bg-gradient-to-r from-[#CC3BD4] to-[#D064AC] font-bold bg-clip-text text-transparent">
+          Smart AI
+        </span>
+      </span>
+      <p className="text-[#989CA0] font-light text-sm">
+        Unlock all features <br /> including AI and more
+      </p>
+      <PaymentButton />
     </div>
-  )
-}
+  );
+};
 
-export default UpgradeCard
+export default UpgradeCard;
