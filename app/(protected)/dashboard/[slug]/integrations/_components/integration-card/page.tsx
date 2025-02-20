01737666c71bab0b1eb2da40754b17644f73c0a2
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-20 10:37:05 +0530

    dev

--- app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
+++ app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 type Props = {
@@ -8,7 +9,25 @@ type Props = {
 };
 
 const IntegrationCard = ({ description, icon, strategy, title }: Props) => {
-  return <div className="border-2 border-[#33"></div>
+  return (
+    <div className="border-2 border-[#3352CC] rounded-2xl gap-x-5 p-5 flex items-center">
+      {icon}
+      <div className="flex flex-col flex-q">
+        <h3 className="text-xl">{title}</h3>
+        <p className="text-[#9D9D9D] text-base w-full md:w-10/12 xl:w-8/12 2xl:w-6/12">
+          {description}
+        </p>
+      </div>
+
+      <Button
+        onClick={onInstaAuth}
+        disabled={integrated?.name === strategy}
+        className="bg-gradient-to-br text-white rounded-full text-lg from-[#3352CC] font-medium to-[#1C2D70] hover:opacity-70 transition duration-100"
+      >
+        {integrated ? "Connected" : "Connect"}
+      </Button>
+    </div>
+  );
 };
 
 export default IntegrationCard;
