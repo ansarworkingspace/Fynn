401830489590d7e2b1aad3125f92c51ad7e3400f
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-21 09:29:40 +0530

    integrations done ui

--- app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
+++ app/(protected)/dashboard/[slug]/integrations/_components/integration-card/page.tsx
@@ -10,9 +10,9 @@ type Props = {
 
 const IntegrationCard = ({ description, icon, strategy, title }: Props) => {
   return (
-    <div className="border-2 border-[#3352CC] rounded-2xl gap-x-5 p-5 flex items-center">
+    <div className="border-2 border-[#3352CC] rounded-2xl gap-x-5 p-5 flex items-center justify-between">
       {icon}
-      <div className="flex flex-col flex-q">
+      <div className="flex flex-col flex-1">
         <h3 className="text-xl">{title}</h3>
         <p className="text-[#9D9D9D] text-base w-full md:w-10/12 xl:w-8/12 2xl:w-6/12">
           {description}
@@ -20,15 +20,15 @@ const IntegrationCard = ({ description, icon, strategy, title }: Props) => {
       </div>
 
       <Button
-        onClick={onInstaAuth}
-        disabled={integrated?.name === strategy}
+        // onClick={onInstaAuth}
+        // disabled={integrated?.name === strategy}
         className="bg-gradient-to-br text-white rounded-full text-lg from-[#3352CC] font-medium to-[#1C2D70] hover:opacity-70 transition duration-100"
       >
-        {integrated ? "Connected" : "Connect"}
+        {/* {integrated ? "Connected" : "Connect"} */}
+        Connect
       </Button>
     </div>
   );
 };
 
 export default IntegrationCard;
-
