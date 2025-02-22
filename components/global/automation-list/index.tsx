b62a96b778087511466a459987cedb3466380416
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-22 10:05:30 +0530

    dev

--- components/global/automation-list/index.tsx
+++ components/global/automation-list/index.tsx
@@ -4,6 +4,7 @@ import { usePaths } from "@/hooks/use-nav";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
+import GradientButton from "../gradient-button";
 
 type Props = {};
 
@@ -22,10 +23,40 @@ const AutomationList = (props: Props) => {
             This is from the comment
           </p>
           <div className="flex gap-x-2 flex-wrap mt-3">
-            <div className={cn("rounded-full px-4 py-1 capitalize")}>
-                
+            <div
+              className={cn(
+                "rounded-full px-4 py-1 capitalize",
+                (0 + 1) % 1 == 0 &&
+                  "bg-keyword-green/15 border-2 border-keyword-green",
+                (1 + 1) % 2 == 0 &&
+                  "bg-keyword-purple/15 border-2 border-keyword-purple",
+                (2 + 1) % 3 == 0 &&
+                  "bg-keyword-yellow/15 border-2 border-keyword-yellow",
+                (3 + 1) % 4 == 0 &&
+                  "bg-keyword-red/15 border-2 border-keyword-red"
+              )}
+            >
+              get started
             </div>
           </div>
+          <div
+            className="rounded-full border-2 mt-3 border-dashed
+          border-white/60 px-3 py-1"
+          >
+            <p className="text-sm text-[#bfc0c3]">No keywords</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          <p className="capitalize text-sm font-light text-[#9B9CA0]">
+            October 5th 2024
+          </p>
+          <GradientButton
+            type="BUTTON"
+            className="w-full bg-background-80 text-white
+           hover:bg-background-80"
+          >
+            SmartAI
+          </GradientButton>
         </div>
       </Link>
     </div>
