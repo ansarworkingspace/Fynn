911501fe075bed679224f44dd4ffdfb1248f74bf
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-26 11:01:09 +0530

    api integration

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,3 +1,5 @@
-export const refreshToken = async(token : string) =>{
+import axios from "axios";
 
-}
+export const refreshToken = async (token: string) => {
+  const refresh_token = await axios.get(`${process.env.INSTAGRAM_BASE_URL}`);
+};
