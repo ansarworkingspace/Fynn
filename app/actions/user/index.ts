3a4b43d2ade439b55bb240d05b61fe952a379a01
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-28 10:30:37 +0530

    dev

--- app/actions/user/index.ts
+++ app/actions/user/index.ts
@@ -2,7 +2,7 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { findUser } from "./queries";
+import { createUser, findUser } from "./queries";
 import { refreshToken } from "@/lib/fetch";
 import { updateIntegration } from "../integrations/queries";
 
@@ -41,17 +41,23 @@ export const onBoardUser = async () => {
       }
 
       return {
-        status:200,
-        data:{
-          firstname:found.firstname,
-          lastname:found.lastname,
-        }
-      }
+        status: 200,
+        data: {
+          firstname: found.firstname,
+          lastname: found.lastname,
+        },
+      };
     }
 
-const created = await createUser()
-
+    const created = await createUser(
+      user.id,
+      user.firstName!,
+      user.lastName!,
+      user.emailAddresses[0].emailAddress
+    );
+    return { status: 201, data: created };
   } catch (error) {
     console.log(error);
+    return { status: 500 };
   }
 };
